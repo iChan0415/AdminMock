dee416d6369d257070babc622638d1ec1e75ddc6
Allada.Joshua <147649070+AlladaJoshua@users.noreply.github.com>
2024-02-12 13:40:44 +0800

    Top bar update

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
+import React from "react";
 import "../css/TopBar.css";
 
 const TopBar = () => {
   return (
     <div>
-        <div className='page-title-container'>
-            <p className='page-title'>
-                Dashboard
-            </p>
-            <div className='page-date'>
-               <p>12th Feb 2024</p> 
-            </div>
-            <div className="admin-profile-container">
-              <a className="profile-settings" href="#">
-                <img
-                  className="admin-profile"
-                  src="src/assets/SatoruGojo.jpg"
-                  alt="Profile"
-                />
-                <p className="admin-name">Caloy Skie</p>
-              </a>
-              <p className="admin-position">Administrator</p>
-            </div>
+      <div className="page-title-container">
+        <div className="header">
+          <p className="page-title">Dashboard</p>
+          <p className="page-date">12th Feb 2024</p>
         </div>
+        <div className="admin-profile-container-top">
+          <div className="profile_top">
+            <a className="profile-settings_top" href="#">
+              <img
+                className="admin-profile"
+                src="src/assets/SatoruGojo.jpg"
+                alt="Profile"
+              />
+            </a>
+          </div>
+          <div className="admin_info_top">
+            <p className="admin-name_top">Caloy Skie</p>
+            <p className="admin-position_top">Administrator</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopBar
+export default TopBar;
