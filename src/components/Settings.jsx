27b0d2274e27691de0219fb39a795cb28fcecd2e
marccarlocruz <154114874+marccarlocruz@users.noreply.github.com>
2024-02-15 09:53:13 +0800

    test commit

---
+++ src/components/Settings.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import "../css/Settings.css";
+
+
+const Settings = () => {
+
+
+
+
+  return (
+    <div class="card-container">
+    <div class="card">
+        <h2>User Management</h2>
+        <p>Manage user accounts, permissions, and roles.</p>
+    </div>
+
+    <div class="card">
+        <h2>Site Settings</h2>
+        <p>Adjust general settings for the site such as logo, title, and theme.</p>
+    </div>
+
+    <div class="card">
+        <h2>Email Configuration</h2>
+        <p>Configure email settings for notifications and user communication.</p>
+    </div>
+
+    <div class="card">
+        <h2>Security</h2>
+        <p>Set up security measures like two-factor authentication and password policies.</p>
+    </div>
+    <div class="card">
+        <h2>Security</h2>
+        <p>Set up security measures like two-factor authentication and password policies.</p>
+    </div>
+    
+</div>
+  );
+}
+
+export default Settings;
--- src/pages/AdminSettings.jsx
+++ src/pages/AdminSettings.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 import SideBar from '../components/SideBar';
 import TopBar from '../components/TopBar';
+import Settings from '../components/Settings'; // Import AdminSettingsButtons
 
 const AdminSettings = () => {
   return (
-    <div><SideBar/>
-    <TopBar pageTitle = "Settings" /></div>
-  )
+    <div>
+      <SideBar />
+      <TopBar pageTitle="Settings" />
+        <Settings /> {/* Render AdminSettingsButtons component */}
+    </div>
+  );
 }
 
-export default AdminSettings
+export default AdminSettings;
